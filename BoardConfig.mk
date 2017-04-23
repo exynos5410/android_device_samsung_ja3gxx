3dd8d251616a011e6171a08c6bfa16864cf9fe9e
Alberto Guanti <albertoguanti@gmail.com>
2017-04-23 12:46:45 +0200

    i9500: Add missing KeyDisabler cmhw class

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,6 +101,7 @@ CHARGING_ENABLED_PATH := "/sys/class/power_supply/battery/batt_lp_charging"
 
 # CMHW
 BOARD_HARDWARE_CLASS := hardware/samsung/cmhw
+BOARD_HARDWARE_CLASS += device/samsung/i9500/cmhw
 
 # Filesystems
 BLOCK_BASED_OTA := false
